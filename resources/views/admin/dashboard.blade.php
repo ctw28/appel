765a078f79cb69c4b54d0ac7e7563eacca0eafe4
rigo <mbandrigo@gmail.com>
2022-06-05 17:33:55 +0800

    pindah laptop

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 <h1>{{$title}}</h1>
-<h2>Selamat Datang, {{Auth::user()->role->keterangan}}</h2>
+<h2>Selamat Datang, {{Auth::user()->role->role_nama}}</h2>
 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
 @csrf
 <button type="submit">Logout</button>
