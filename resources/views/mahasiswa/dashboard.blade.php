3a65f690939b5473c1a848a67c26bdece1d4df60
rigo mac <mbandrigo@gmail.com>
2023-07-13 09:01:33 +0800

    update

--- resources/views/mahasiswa/dashboard.blade.php
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -14,8 +14,38 @@
                 </div>
             </div>
             <div class="card-body p-3 pt-0">
-                {{$data}}
-
+                <ul class="list-group mb-2">
+                    <li class="list-group-item border-0 ps-0 text-sm">Nama Kuliah : &nbsp; <strong class="text-dark">{{$data->kuliahLapangan->kuliah_lapangan_nama}} - {{$data->kuliahLapangan->tahunAkademik->sebutan}}</strong></li>
+                    <li class="list-group-item border-0 ps-0 text-sm">Waktu Pelaksanaan : &nbsp; <strong class="text-dark">{{$data->kuliahLapangan->waktu_pelaksanaan_mulai}} - {{$data->kuliahLapangan->waktu_pelaksanaan_selesai}}</strong></li>
+                    <h6 class="mt-2">Informasi Kelompok</h6>
+                    @if(!empty($data->anggota))
+                    <li class="list-group-item border-0 ps-0 text-sm">Lokasi : &nbsp; <strong class="text-dark">{{$data->anggota->kelompok->lokasi->lokasi}}</strong></li>
+                    <li class="list-group-item border-0 ps-0 text-sm">Kelompok : &nbsp; <strong class="text-dark">{{$data->anggota->kelompok->nama_kelompok}}</strong></li>
+                    <li class="list-group-item border-0 ps-0 text-sm">Pembimbing : &nbsp; <strong class="text-dark">
+                            @if($data->anggota->kelompok->pembimbing->pegawai->gelar->gelar_depan!="-")
+                            {{$data->anggota->kelompok->pembimbing->pegawai->gelar->gelar_depan}}
+                            @endif
+                            {{$data->anggota->kelompok->pembimbing->pegawai->dataDiri->nama_lengkap}}
+                            {{$data->anggota->kelompok->pembimbing->pegawai->gelar->gelar_belakang}}
+                        </strong></li>
+                    @else
+                    <li class="list-group-item border-0 ps-0 text-sm">Lokasi : &nbsp; <strong class="text-dark">Mohon menunggu...</strong></li>
+                    <li class="list-group-item border-0 ps-0 text-sm">Kelompok : &nbsp; <strong class="text-dark">Mohon menunggu...</strong></li>
+                    <li class="list-group-item border-0 ps-0 text-sm">Pembimbing : &nbsp; <strong class="text-dark">Mohon menunggu...</strong></li>
+                    @endif
+                </ul>
+                <div>
+                    @if(!empty($data->anggota))
+                    <a href="{{route('mahasiswa.lkh', $data->kuliahLapangan->id)}}" class="btn btn-primary" style="margin-right:10px"><i class="material-icons">post_add</i> Isi LKH</a>
+                    <a href="{{route('mahasiswa.kelompok.detail',$data->anggota->kelompok->id)}}" class="btn btn-success"><i class="material-icons">info</i> Detail Kelompok</a>
+                    @else
+                    <a href="#" class="btn btn-primary"><i class="material-icons">post_add</i> Isi LKH</a>
+                    <a href="#" class="btn btn-secondary"><i class="material-icons">info</i> Detail Kelompok</a>
+                    @endif
+                </div>
+                @else
+                <p>Anda tidak mengikuti {{session('fakultasData')->singkatan}} untuk saat ini</p>
+                @endif
 
             </div>
         </div>
