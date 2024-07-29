bcbfeeb1222d13e7aca23522ab1b92d1c6a5f8b0
rigo mac <mbandrigo@gmail.com>
2024-07-29 20:01:13 +0800

    tambah lkh

--- app/Http/Controllers/Mahasiswa/MahasiswaController.php
+++ app/Http/Controllers/Mahasiswa/MahasiswaController.php
@@ -273,7 +273,7 @@ class MahasiswaController extends Controller
                     'anggota_id' => 'required',
                     'kegiatan' => 'required',
                     'tgl_lkh' => 'required',
-                    'photos.*' => 'required|image|mimes:jpeg,png,jpg|file|max:1024',
+                    'photos.*' => 'required|image|mimes:jpeg,png,jpg|file|max:2048',
                     // 'photos' => 'max:3'
                 ],
                 [
@@ -282,7 +282,7 @@ class MahasiswaController extends Controller
                     'tgl_lkh.required' => 'Tanggal LKH Tidak boleh kosong',
                     'photos.*.required' => 'Foto tidak boleh kosong',
                     'photos.*.mimes' => 'File hanya boleh jpeg, png atau jpg',
-                    'photos.*.max' => 'Ukuran file tidak boleh lebih dari 1024kb',
+                    'photos.*.max' => 'Ukuran file tidak boleh lebih dari 2048kb',
                 ]
             );
             if ($validator->fails()) {
