146536a37ddfeb9b741eb3b1fd883f0303afb099
rigo mac <mbandrigo@gmail.com>
2025-02-27 15:23:02 +0800

    update

--- app/Http/Controllers/KuliahLapanganController.php
+++ app/Http/Controllers/KuliahLapanganController.php
@@ -29,7 +29,7 @@ class KuliahLapanganController extends Controller
                 $ppl->where('master_fakultas_id', Auth::user()->userFakultas->master_fakultas_id);
             })->where('is_active', 1);
         }])
-            // ->where('tahun', 2024)
+            ->where('tahun', 2024)
             ->orderBy('id', "DESC")
             ->first();
         // return $data;
