def5847e7dbf688f7fd58ac122891aa3d9eb4992
rigo mac <mbandrigo@gmail.com>
2025-07-21 14:42:34 +0800

    icon tidak tampil solve

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -12,7 +12,9 @@
     <link href="{{asset('/')}}assets/css/nucleo-icons.css" rel="stylesheet" />
     <link href="{{asset('/')}}assets/css/nucleo-svg.css" rel="stylesheet" />
     <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <!-- CSS Files -->
